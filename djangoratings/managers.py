d5d77b45f49abef3e6b5ef34859405490b4a8742
Stian Prestholdt <stian@e5r.no>
2017-03-07 11:32:09 +0100

    Update to Django 1.7+ compatible code

--- djangoratings/managers.py
+++ djangoratings/managers.py
@@ -11,25 +11,25 @@ class VoteQuerySet(QuerySet):
         from .fields import RatingField
 
         qs = self.distinct().values_list('content_type', 'object_id').order_by('content_type')
-    
+
         to_update = []
         for content_type, objects in itertools.groupby(qs, key=lambda x: x[0]):
             model_class = ContentType.objects.get(pk=content_type).model_class()
             if model_class:
                 to_update.extend(list(model_class.objects.filter(pk__in=list(objects)[0])))
-        
+
         retval = super(VoteQuerySet, self).delete(*args, **kwargs)
-        
+
         # TODO: this could be improved
         for obj in to_update:
             for field in getattr(obj, '_djangoratings', []):
                 getattr(obj, field.name)._update(commit=False)
             obj.save()
-        
+
         return retval
-        
+
 class VoteManager(Manager):
-    def get_query_set(self):
+    def get_queryset(self):
         return VoteQuerySet(self.model)
 
     def get_for_user_in_bulk(self, objects, user):
@@ -48,16 +48,16 @@ class VoteManager(Manager):
 class SimilarUserManager(Manager):
     def get_recommendations(self, user, model_class, min_score=1):
         from djangoratings.models import Vote, IgnoredObject
-        
+
         content_type = ContentType.objects.get_for_model(model_class)
-        
+
         params = dict(
             v=Vote._meta.db_table,
             sm=self.model._meta.db_table,
             m=model_class._meta.db_table,
             io=IgnoredObject._meta.db_table,
         )
-        
+
         objects = model_class._default_manager.extra(
             tables=[params['v']],
             where=[
@@ -79,9 +79,9 @@ class SimilarUserManager(Manager):
         # ).exclude(
         #     object_id__in=Vote.objects.filter(content_type=content_type, user=user).values_list('object_id', flat=True)
         # ).distinct().values_list('object_id', flat=True))
-        
+
         return objects
-    
+
     def update_recommendations(self):
         # TODO: this is mysql only atm
         # TODO: this doesnt handle scores that have multiple values (e.g. 10 points, 5 stars)
