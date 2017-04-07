de63d82514b337f7477b4a7fe149363f4a5c1dae
Frode Danielsen <frode@e5r.no>
2017-04-07 12:27:54 +0200

    Bump version to 0.4.1

--- djangoratings/__init__.py
+++ djangoratings/__init__.py
@@ -2,7 +2,7 @@ import os.path
 import warnings
 import collections
 
-__version__ = (0, 4, 0)
+__version__ = (0, 4, 1)
 
 def _get_git_revision(path):
     revision_file = os.path.join(path, 'refs', 'heads', 'master')
